fa3162243a0c0ca050b84c83f75639eb7139f99f
Marcus Bätz <s59156@beuth-hochschule.de>
2016-05-21 20:19:29 +0200

    GetCoordinateTool dem Repository hinzugefügt

---
+++ get_coordinate/app/src/main/java/com/projecttango/examples/java/getCoordinate/GetCoordinateActivity.java
@@ -0,0 +1,261 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.projecttango.examples.java.getCoordinate;
+
+import com.google.atap.tangoservice.Tango;
+import com.google.atap.tangoservice.Tango.OnTangoUpdateListener;
+import com.google.atap.tangoservice.TangoConfig;
+import com.google.atap.tangoservice.TangoCoordinateFramePair;
+import com.google.atap.tangoservice.TangoErrorException;
+import com.google.atap.tangoservice.TangoEvent;
+import com.google.atap.tangoservice.TangoOutOfDateException;
+import com.google.atap.tangoservice.TangoPoseData;
+import com.google.atap.tangoservice.TangoXyzIjData;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.rajawali3d.surface.IRajawaliSurface;
+import org.rajawali3d.surface.RajawaliSurfaceView;
+
+import java.util.ArrayList;
+
+/**
+ * Main Activity class for the Motion Tracking API Sample. Handles the connection to the Tango
+ * service and propagation of Tango pose data to OpenGL and Layout views. OpenGL rendering logic is
+ * delegated to the {@link GetCoordinateRenderer} class.
+ */
+public class GetCoordinateActivity extends Activity {
+
+    private static final String TAG = GetCoordinateActivity.class.getSimpleName();
+    private static final int SECS_TO_MILLISECS = 1000;
+    private static final double UPDATE_INTERVAL_MS = 100.0;
+
+    private double mPreviousPoseTimeStamp;
+    private double mTimeToNextUpdate = UPDATE_INTERVAL_MS;
+
+    private Tango mTango;
+    private TangoConfig mConfig;
+    private GetCoordinateRenderer mRenderer;
+    private final Object mSharedLock = new Object();
+    private boolean mIsRelocalized;
+
+    private TextView txtLocalized;
+    private TangoPoseData poseData = new TangoPoseData();
+    // Handles the debug text UI update loop.
+    private Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_coordinate);
+
+        mRenderer = setupGLViewAndRenderer();
+
+        // Check the current screen rotation and set it to the renderer.
+        WindowManager mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        Display mDisplay = mWindowManager.getDefaultDisplay();
+        mRenderer.setCurrentScreenRotation(mDisplay.getRotation());
+        txtLocalized = (TextView) findViewById(R.id.txtLocalized);
+        startActivityForResult(
+                Tango.getRequestPermissionIntent(Tango.PERMISSIONTYPE_ADF_LOAD_SAVE), 0);
+
+
+    }
+
+    /**
+     * Sets Rajawalisurface view and its renderer. This is ideally called only once in onCreate.
+     */
+    private GetCoordinateRenderer setupGLViewAndRenderer() {
+        // Configure OpenGL renderer
+        GetCoordinateRenderer renderer = new GetCoordinateRenderer(this);
+        // OpenGL view where all of the graphics are drawn
+        RajawaliSurfaceView glView = (RajawaliSurfaceView) findViewById(R.id.gl_surface_view);
+        glView.setEGLContextClientVersion(2);
+        glView.setRenderMode(IRajawaliSurface.RENDERMODE_CONTINUOUSLY);
+        glView.setSurfaceRenderer(renderer);
+        return renderer;
+
+    }
+
+    /**
+     * Sets up the tango configuration object. Make sure mTango object is initialized before
+     * making this call.
+     */
+    private TangoConfig setupTangoConfig(Tango tango) {
+        // Create a new Tango Configuration and enable the GetCoordinateActivity API
+        TangoConfig config;
+        config = tango.getConfig(TangoConfig.CONFIG_TYPE_DEFAULT);
+        config.putBoolean(TangoConfig.KEY_BOOLEAN_MOTIONTRACKING, true);
+
+        // Tango service should automatically attempt to recover when it enters an invalid state.
+        config.putBoolean(TangoConfig.KEY_BOOLEAN_AUTORECOVERY, true);
+
+        config.putBoolean(TangoConfig.KEY_BOOLEAN_DEPTH, true);
+        config.putBoolean(
+                TangoConfig.KEY_BOOLEAN_LOWLATENCYIMUINTEGRATION, true);
+
+        ArrayList<String> fullUuidList;
+        // Returns a list of ADFs with their UUIDs
+        fullUuidList = tango.listAreaDescriptions();
+        // Load the latest ADF if ADFs are found.
+        if (fullUuidList.size() > 0) {
+            config.putString(TangoConfig.KEY_STRING_AREADESCRIPTION,
+                    fullUuidList.get(fullUuidList.size() - 1));
+        }
+
+        return config;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mIsRelocalized = false;
+        mRenderer.onPause();
+        try {
+            mTango.disconnect();
+        } catch (TangoErrorException e) {
+            Toast.makeText(getApplicationContext(),
+                    R.string.exception_tango_error, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mRenderer.onResume();
+        // Initialize Tango Service as a normal Android Service, since we call 
+        // mTango.disconnect() in onPause, this will unbind Tango Service, so
+        // everytime when onResume get called, we should create a new Tango object.
+
+        mTango = new Tango(GetCoordinateActivity.this, new Runnable() {
+            // Pass in a Runnable to be called from UI thread when Tango is ready,
+            // this Runnable will be running on a new thread.
+            // When Tango is ready, we can call Tango functions safely here only
+            // when there is no UI thread changes involved.
+            @Override
+            public void run() {
+                mConfig = setupTangoConfig(mTango);
+
+                try {
+                    setUpTangoListeners();
+                } catch (TangoErrorException e) {
+                    Log.e(TAG, "Tango Error", e);
+                } catch (SecurityException e) {
+                    Log.e(TAG, "No Permission", e);
+                }
+                try {
+                    mTango.connect(mConfig);
+                } catch (TangoOutOfDateException e) {
+                    Log.e(TAG, getString(R.string.exception_out_of_date), e);
+                } catch (TangoErrorException e) {
+                    Log.e(TAG, getString(R.string.exception_tango_error), e);
+                }
+            }
+        });
+        mHandler.post(mUpdateUiLoopRunnable);
+    }
+
+    /**
+     * Set up the callback listeners for the Tango service, then begin using the Motion
+     * Tracking API. This is called in response to the user clicking the 'Start' Button.
+     */
+    private void setUpTangoListeners() {
+        // Set Tango Listeners for Poses Device wrt Start of Service, Device wrt
+        // ADF and Start of Service wrt ADF
+        ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<TangoCoordinateFramePair>();
+        framePairs.add(new TangoCoordinateFramePair(
+                TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+                TangoPoseData.COORDINATE_FRAME_DEVICE));
+        framePairs.add(new TangoCoordinateFramePair(
+                TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION,
+                TangoPoseData.COORDINATE_FRAME_DEVICE));
+        framePairs.add(new TangoCoordinateFramePair(
+                TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION,
+                TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE));
+
+        mTango.connectListener(framePairs, new OnTangoUpdateListener() {
+            @Override
+            public void onXyzIjAvailable(TangoXyzIjData xyzij) {
+                if (mIsRelocalized) {
+                    Log.i("Debugg", "xyz-Count: " + xyzij.xyzCount + "");
+                }
+            }
+
+            // Listen to Tango Events
+            @Override
+            public void onTangoEvent(final TangoEvent event) {
+            }
+
+            @Override
+            public void onPoseAvailable(TangoPoseData pose) {
+                poseData = pose;
+                // Make sure to have atomic access to Tango Data so that
+                // UI loop doesn't interfere while Pose call back is updating
+                // the data.
+                synchronized (mSharedLock) {
+                    // Check for Device wrt ADF pose, Device wrt Start of Service pose,
+                    // Start of Service wrt ADF pose (This pose determines if the device
+                    // is relocalized or not).
+                    if (pose.baseFrame == TangoPoseData.COORDINATE_FRAME_AREA_DESCRIPTION
+                            && pose.targetFrame == TangoPoseData
+                            .COORDINATE_FRAME_START_OF_SERVICE) {
+                        mIsRelocalized = pose.statusCode == TangoPoseData.POSE_VALID;
+
+                    }
+                }
+
+            }
+
+            @Override
+            public void onFrameAvailable(int cameraId) {
+                // We are not using onFrameAvailable for this application.
+            }
+        });
+    }
+
+    private Runnable mUpdateUiLoopRunnable = new Runnable() {
+        public void run() {
+            final double deltaTime = (poseData.timestamp - mPreviousPoseTimeStamp) *
+                    SECS_TO_MILLISECS;
+            mPreviousPoseTimeStamp = poseData.timestamp;
+            mTimeToNextUpdate -= deltaTime;
+
+            if (mTimeToNextUpdate < 0.0) {
+                mTimeToNextUpdate = UPDATE_INTERVAL_MS;
+
+
+                if (mIsRelocalized) txtLocalized.setText(
+                        "Localized");
+            }
+
+
+            mHandler.postDelayed(this, 100);
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ get_coordinate/app/src/main/java/com/projecttango/examples/java/getCoordinate/GetCoordinateRenderer.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.projecttango.examples.java.getCoordinate;
+
+import com.google.atap.tangoservice.TangoErrorException;
+import com.google.atap.tangoservice.TangoPoseData;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.MotionEvent;
+
+import org.rajawali3d.math.Quaternion;
+import org.rajawali3d.math.vector.Vector3;
+import org.rajawali3d.renderer.RajawaliRenderer;
+
+import com.projecttango.rajawali.Pose;
+import com.projecttango.rajawali.renderables.Grid;
+
+import com.projecttango.tangosupport.TangoSupport;
+
+/**
+ * This class implements the rendering logic for the Motion Tracking application using Rajawali.
+ */
+public class GetCoordinateRenderer extends RajawaliRenderer {
+    private static final String TAG = GetCoordinateRenderer.class.getSimpleName();
+
+    private static final float CAMERA_NEAR = 0.01f;
+    private static final float CAMERA_FAR = 200f;
+
+    // Latest available device pose;
+    private Pose mDevicePose = new Pose(Vector3.ZERO, Quaternion.getIdentity());
+    private boolean mPoseUpdated = false;
+  
+    // The current screen rotation index. The index value follow the Android surface rotation enum:
+    // http://developer.android.com/reference/android/view/Surface.html#ROTATION_0
+    private int mCurrentScreenRotation = 0;
+
+    public GetCoordinateRenderer(Context context) {
+        super(context);
+    }
+
+    public void setCurrentScreenRotation(int currentRotation) {
+        mCurrentScreenRotation = currentRotation;
+        //Zeigt an ob Tablet in Hoch oder querformat und wie herum
+    }
+  
+    @Override
+    protected void initScene() {
+        Grid grid = new Grid(100, 1, 1, 0xFFCCCCCC);
+        grid.setPosition(0, -1.3f, 0);
+        getCurrentScene().addChild(grid);
+
+        getCurrentScene().setBackgroundColor(Color.WHITE);
+
+        getCurrentCamera().setNearPlane(CAMERA_NEAR);
+        getCurrentCamera().setFarPlane(CAMERA_FAR);
+    }
+
+    @Override
+    protected void onRender(long ellapsedRealtime, double deltaTime) {
+        // Update the scene objects with the latest device position and orientation information.
+        // Synchronize to avoid concurrent access from the Tango callback thread below.
+        try {
+            TangoPoseData pose =
+                TangoSupport.getPoseAtTime(0.0, TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+                                     TangoPoseData.COORDINATE_FRAME_DEVICE,
+                                     TangoSupport.TANGO_SUPPORT_ENGINE_OPENGL,
+                                     mCurrentScreenRotation);
+            if (pose.statusCode == TangoPoseData.POSE_VALID) {
+                getCurrentCamera().setPosition((float) pose.translation[0],
+                                               (float) pose.translation[1],
+                                               (float) pose.translation[2]);
+            
+        
+                Quaternion invOrientation = new Quaternion((float) pose.rotation[3],
+                                                            (float) pose.rotation[0],
+                                                            (float) pose.rotation[1],
+                                                            (float) pose.rotation[2]);
+
+                // For some reason, rajawalli's orientation is inversed.
+                Quaternion orientation = invOrientation.inverse();
+                getCurrentCamera().setOrientation(orientation);   
+            }
+        } catch (TangoErrorException e) {
+            Log.e(TAG, "TangoSupport.getPoseAtTime error", e);
+        }
+
+        // Perform the actual OpenGL rendering of the updated objects
+        super.onRender(ellapsedRealtime, deltaTime);
+    }
+
+    @Override
+    public void onOffsetsChanged(float v, float v1, float v2, float v3, int i, int i1) {
+      // Unused, but needs to be declared to adhere to the IRajawaliSurfaceRenderer interface.
+    }
+
+    @Override
+    public void onTouchEvent(MotionEvent motionEvent) {
+      // Unused, but needs to be declared to adhere to the IRajawaliSurfaceRenderer interface.
+    }
+}
